7cf32fcf314f982ccf797e5504d156e13b2dc8b3
emmanuel123 <obaloluwaajayi2006@gmail.com>
2026-05-18 19:04:57 -0700

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
             <div className='btn-one btn'>• Next Generation Edtech infrastructure</div>
             <h1 className='hero-heading'>The <span>Operating <br /> System</span> for modern <br /> institutions.</h1>
             <h6 className='hero-subheading'>MedxLearn builds world-class digital platforms for universities, colleges nursing <br /> schools, and training institutes, integrating admissions, LMS payments, AI tools, <br /> and institutional automation into one seamless ecosytem.</h6>
+            div
           </div>
         </div>
       </section>
