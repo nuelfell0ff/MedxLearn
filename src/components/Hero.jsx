6aecb516decfae9867d5464eaa07655b9d6285c3
emmanuel123 <obaloluwaajayi2006@gmail.com>
2026-05-18 19:12:38 -0700

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,10 @@ const Hero = () => {
             <div className='btn-one btn'>• Next Generation Edtech infrastructure</div>
             <h1 className='hero-heading'>The <span>Operating <br /> System</span> for modern <br /> institutions.</h1>
             <h6 className='hero-subheading'>MedxLearn builds world-class digital platforms for universities, colleges nursing <br /> schools, and training institutes, integrating admissions, LMS payments, AI tools, <br /> and institutional automation into one seamless ecosytem.</h6>
-            div
+            <div className='d-flex gap-2'>
+              <button className='btn hero-btnone'>Get a Free Demo</button>
+              <button className='btn hero-btntwo'>Explore Platform</button>
+            </div>
           </div>
         </div>
       </section>
